674da2aa7922470670679c072420f5153aa8ad19
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-12-05 08:07:54 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="hero min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello World</h1>
+            <p className="py-6">This is a basic React + Vite project with Tailwind CSS and DaisyUI preconfigured.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
